5c87491ef0e0720542d3cb30ca88e2dd28922e55
Helman Mgidi <thembamgidi8@gmail.com>
2024-12-16 15:25:06 +0200

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Defining variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculating interest
+interest = principal * rate * time
+
+# Printing result
+print(f"The simple interest is: {interest}")
