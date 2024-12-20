fb329aa631e6dc6d50a81ec8883e1d77315ee1e8
Helman Mgidi <thembamgidi8@gmail.com>
2024-12-20 19:27:27 +0200

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,15 +6,13 @@ def main():
 
     # Provide clothing recommendations based on the weather input
     if weather == "sunny":
-        message = ("Wear a t-shirt and sunglasses.")
+        print("Wear a t-shirt and sunglasses.")
     elif weather == "rainy":
-        message = ("Don't forget your umbrella and a raincoat.")
+        print("Don't forget your umbrella and a raincoat.")
     elif weather == "cold":
-        message = ("Make sure to wear a warm coat and a scarf.")
+        print("Make sure to wear a warm coat and a scarf.")
     else:
-        message = ("Sorry, I don't have recommendations for this weather.")
-
-    print (message)
+        print("Sorry, I don't have recommendations for this weather.")
 
 # Run the main function
 if __name__ == "__main__":
